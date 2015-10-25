c4d17d911c4da9e952b58182748c4e16c20ca9c8
mshr <mshr@localhost.localdomain>
2015-10-26 07:58:49 +0900

    chat tutorial2

--- socketio/modules/SocketIo.js
+++ socketio/modules/SocketIo.js
@@ -1,3 +1,4 @@
+nickNames = {};
 exports.socketIoListen = function(http) {
   var io = require('socket.io')(http);
 
@@ -6,24 +7,48 @@ exports.socketIoListen = function(http) {
     console.log('connection', socket.id);
 
     socket.on('all', function(data) {
-      var message = data.message;
-      io.sockets.emit('all', message);
+      io.sockets.emit('message', makeMessage(socket.id, data));
     });
 
     socket.on('join', function(data) {
+      nickNames[socket.id] = data.nickName;
+      console.log(nickNames[socket.id]);
       var roomName = data.roomName;
       socket.join(roomName);
     });
 
     socket.on('room', function(data) {
-      var roomName = data.roomName;
-      var message = data.message;
-      io.sockets.to(roomName).emit('room', message);
+      io.sockets.to(data.roomName).emit('message', makeMessage(socket.id, data));
     });
 
     socket.on("disconnect", function() {
       console.log('disconnect', socket.id);
+      delete nickNames[socket.id];
     });
 
   });
+}
+
+/*
+ * 日付を取得
+ */
+function getDate() {
+  var date  = new Date();
+  date      = new Date(date.getTime() + 9 * 60 * 60 * 1000);
+
+  var month   = ("0" + (date.getMonth() + 1)).slice(-2);
+  var day     = ("0" + date.getDate()).slice(-2);
+  var hour    = ("0" + date.getHours()).slice(-2);
+  var minute  = ("0" + date.getMinutes()).slice(-2);
+  var second  = ("0" + date.getSeconds()).slice(-2);
+  // return (month+"/"+day+" "+hour+":"+minute+":"+second);
+  return (hour+":"+minute+":"+second);
+}
+
+function makeMessage(id, data) {
+  return {
+    nickName: nickNames[id],
+    message:  data.message,
+    time:     getDate()
+  };
 }
--- socketio/routes/room.js
+++ socketio/routes/room.js
@@ -1,16 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var stash = express();
 
 router.get('/', function(req, res) {
   res.render('room/index', { title: 'room' });
 });
 
 router.post('/', function(req, res) {
+  stash.set('nickName', req.body.nickName);
   res.redirect(req.body.roomName);
 });
 
 router.all('/:roomName', function(req, res) {
-  res.render('room/room', { roomName: req.params.roomName });
+  res.render('room/room', { roomName: req.params.roomName, nickName: stash.get('nickName') });
 });
 
 module.exports = router;
